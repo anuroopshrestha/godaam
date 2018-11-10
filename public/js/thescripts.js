5a8dd9a2230906fa54e9764ed918d031d19664e1
Roshan Maharjan <roshan.bodom@gmail.com>
2018-11-10 12:43:53 +0545

    sidebar submenu bootstrap scss

--- public/js/thescripts.js
+++ public/js/thescripts.js
@@ -1,5 +1,31 @@
 const $ = require('jquery');
+const cookie = require('js-cookie');
+
+$(document).ready(function() {
+  const checkCookie = cookie.get('portalmenu');
+  if (checkCookie) {
+    $('body').addClass('menu-closed');
+  }
+});
 
 $('#menu-clicker span').click(function() {
-  $('body').toggleClass('menu-closed');
+  const body = $('body');
+  if (body.hasClass('menu-closed')) {
+    body.removeClass('menu-closed');
+    cookie.remove('portalmenu', { expires: 7, path: '/' });
+  } else {
+    body.addClass('menu-closed');
+    $('.submenu').removeClass('open');
+    cookie.set('portalmenu', '1', { expires: 7, path: '/' });
+  }
+});
+
+$('a.withChild').click(function(e) {
+  e.preventDefault();
+  if ($(this).siblings('ul').hasClass('open')) {
+    $(this).siblings('ul').removeClass('open');
+  } else {
+    $('.submenu').removeClass('open');
+    $(this).siblings('ul').addClass('open');
+  }
 });
