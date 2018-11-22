d6785ac62dab7be4644492f23cf1f85ae950a87b
anuroop shrestha <anuroopshr@gmail.com>
2018-11-22 13:36:55 +0545

    Brands Add

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,49 +1,19 @@
 const mongoose = require('mongoose');
-const Brand = mongoose.model('Store');
-const storeController = require('./storeController');
-
-exports.brandsPage = async(req, res) => {
-  res.render('brands/all', {title: 'Brands'});
-};
+const Store = mongoose.model('Store');
 
 exports.addBrand = async (req, res) => {
-  if (!req.body.store) {
-    req.body.store = req.user._id;
-  }
-  const brand = new Brand(req.body);
-  await brand.save();
-  req.flash('info', 'New brand has been added!');
-  res.redirect('/brands');
-  req.flash('error', 'An unexpected error has occured.');
-  res.redirect('/brands');
+  await Store.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      $push: {
+        brands: req.body
+      }
+    }
+  );
+  req.flash('info', 'Brand has been added successfully');
+  res.redirect(`/store/${req.params.id}`);
 };
 
-exports.editBrand = async (req, res) => {
-  const brand = await Brand.findOne({ _id: req.params.id});
-  // res.json(brand);
-  res.render('brands/edit', {title: `Edit ${brand.name}`, brand});
-};
-
-exports.updateBrand = async (req, res) => {
-  console.log('asd');
-  // find and update the brand
-  const brand = await Brand.findOneAndUpdate({ _id: req.params.id}, req.body);
-  req.flash('info', `Successfully updated ${brand.name}`);
-  res.redirect('/brands');
-};
-
-exports.deleteBrand = async (req, res) => {
-  const brand = await Brand.findByIdAndRemove({ _id: req.params.id}, req.body);
-  req.flash('info', `Successfully deleted ${brand.name}`);
-  res.redirect('/brands');
-};
-
-// exports.getBrandList = async => {
-//   const brands = await Brand.find({name});
-//   return brands;
-// };
-
-exports.getBrandList = async () => {
-  const stores = await Brand.find({name: {}});
-  return stores;
-};
+exports.editBrand = (req, res) => {
+  console.log('EDIT');
+}
