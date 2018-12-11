5e15e90a022b4ae56fbbfbc3607feb4cc2b5db17
anuroop shrestha <anuroopshr@gmail.com>
2018-12-11 18:34:23 +0545

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 // const mail = require('../handlers/mailHandler');
 
@@ -37,4 +40,80 @@ exports.isLoggedIn = (req, res, next) => {
     res.redirect('/login');
   }
   return false;
+};
+
+const getToken = (user) => {
+  const payload = {
+    email: user.email
+  };
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
+  return token;
+};
+
+exports.apiLogin = (req, res, next) => {
+  passport.authenticate('local', function(err, user) {
+    if (err) {
+      const response = {
+        status: 'error',
+        message: 'Unknown Error'
+      };
+      res.json(response);
+      return;
+    }
+    if (!user) {
+      const response = {
+        status: 'auth-error',
+        message: 'The email and/or password is wrong. Please try again'
+      };
+      res.json(response);
+      return;
+    }
+    if (user.verified.status === 'false') {
+      const response = {
+        status: 'not-verified',
+        message: 'Your email has not been verified yet. Please check your email for verification code'
+      };
+      res.json(response);
+      return;
+    }
+    req.user = user;
+    next();
+  })(req, res, next);
+};
+
+exports.apiAuthenticate = passport.authenticate('jwt', { session: 'false' });
+
+exports.refreshToken = (req, res) => {
+  res.json({
+    status: 'success',
+    user: {
+      name: req.user.name,
+      email: req.user.email,
+      id: req.user._id,
+      picture: req.user.picture,
+      updated: req.user.verified.profileUpdated,
+      follows: req.user.follows,
+      role: req.user.role,
+      initiateFollows: req.user.verified.initiateFollows,
+      notifications: req.user.notifications
+    }
+  });
+};
+
+exports.sendToken = (req, res) => {
+  res.json({
+    status: 'success',
+    token: getToken(req.user),
+    user: {
+      name: req.user.name,
+      email: req.user.email,
+      id: req.user._id,
+      picture: req.user.picture,
+      updated: req.user.verified.profileUpdated,
+      follows: req.user.follows,
+      role: req.user.role,
+      initiateFollows: req.user.verified.initiateFollows,
+      notifications: req.user.notifications
+    }
+  });
 };
